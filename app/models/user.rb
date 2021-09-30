29ad149471a2c9eabc3581fcbb9393063df7f5c0
“iwasawa <iwasi_44@hotmail.com>
2021-09-30 12:48:05 +0900

    ユーザー登録実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   has_many :pictures
-  validate :name, presence: true, length: { maximum: 30 }
-  validate :email, presence: true, length: { maximum: 1..255 },
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 },
                   format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                   uniqueness: true
   before_validation { email.downcase!}
   has_secure_password
-  validate :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2021_09_30_001926) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password_gigest"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
