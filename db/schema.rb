35a3d113ad09ba441de04cc8956092c7ac2a5263
“iwasawa <iwasi_44@hotmail.com>
2021-09-30 09:52:32 +0900

    user validate設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_29_233533) do
+ActiveRecord::Schema.define(version: 2021_09_30_001926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2021_09_29_233533) do
     t.string "password_gigest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "pictures", "users"
