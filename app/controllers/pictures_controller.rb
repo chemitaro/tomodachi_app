716072124c38ab694462b3f6537a3b9d1685938c
“iwasawa <iwasi_44@hotmail.com>
2021-10-01 11:05:03 +0900

    画像投稿機能実装

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -16,7 +16,7 @@ class PicturesController < ApplicationController
 
   private
   def picture_params
-    params.require(:picture).permit(:title, :content, :image)
+    params.require(:picture).permit(:title, :content, :image, :image_cache)
   end
 
 end
