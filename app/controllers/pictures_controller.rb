910ec3b58a1f63b912b3e10089dfa5c22ac6df35
“iwasawa <iwasi_44@hotmail.com>
2021-10-01 12:24:22 +0900

    投稿一覧にユーザー名を表示

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,18 +1,32 @@
 class PicturesController < ApplicationController
   def index
     @pictures = Picture.all
+    @users = User.select(:id, :name)
   end
   def new
     @picture = Picture.new
   end
   def create
     @picture = Picture.new(picture_params)
+    @picture.user_id = current_user.id
     if @picture.save
-      redirect_to picturs_path
+      redirect_to pictures_path
     else
       render :new
     end
   end
+  def show
+    @picture = Picture.find(params[:id])
+  end
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+  def update
+    
+  end
+  
+  
+  
 
   private
   def picture_params
