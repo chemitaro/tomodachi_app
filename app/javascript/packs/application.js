4b79494004a3a19006c06c4b50f756c3761ac98d
“iwasawa <iwasi_44@hotmail.com>
2021-10-01 16:08:53 +0900

    js修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,23 +3,29 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-
-
-import '~bulma'
-import 'bootstrap'
-import '../stylesheets/application'
-// import Rails from "@rails/ujs"
-// import Turbolinks from "turbolinks"
-// import * as ActiveStorage from "@rails/activestorage"
-// import "channels"
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels").
-require jquery
-require jquery.turbolinks
-require jquery_ujs
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
+// import '~bulma'
+// import 'bootstrap'
+// import '../stylesheets/application'
+// // import Rails from "@rails/ujs"
+// // import Turbolinks from "turbolinks"
+// // import * as ActiveStorage from "@rails/activestorage"
+// // import "channels"
+// require("@rails/ujs").start()
+// require("turbolinks").start()
+// require("@rails/activestorage").start()
+// require("channels").
+
+
+// Rails.start()
+// Turbolinks.start()
+// ActiveStorage.start()
